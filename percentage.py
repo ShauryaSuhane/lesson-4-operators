b0ad1d9e2d1b4a079f883e694ce91e7ed059cf78
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-05-21 10:33:58 +0530

    percentage

---
+++ percentage.py
@@ -0,0 +1,7 @@
+maths=int(input("enter the marks in maths:"))
+hindi=int(input("enter the marks in hindi:"))
+science=int(input("enter the marks in science:"))
+english=int(input("enter the marks in english:"))
+sum=(maths+hindi+science+english)
+percent=(sum/400)*100
+print("the percentage is",percent)
